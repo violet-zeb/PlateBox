a68383f645be7e211af5715e41d1406b54cc8cb7
Stef PC <Stef PC@DESKTOP-VAA867M>
2022-03-23 22:17:41 +0100

    Add project files.

---
+++ PlateBox/PlateBox/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Prism;
+
+namespace PlateBox
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+            Task.Run(async () =>
+            {
+                this.BackgroundColor = Color.Black;
+
+                await Task.Delay(3000);
+
+                this.BackgroundColor = Color.White;
+
+            }).Wait();
+        }
+
+
+        private  void Button_Clicked(object sender, EventArgs e)
+        {
+            var mergetDictionaries =
+                Application.Current.Resources.MergedDictionaries;
+            mergetDictionaries.Clear();
+            
+        }
+    }
+}
